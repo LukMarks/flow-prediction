0453fd26cb2839bf4c765971b5b77af18ad21047
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-08 14:59:31 -0300

    Convert to pub and fixed typo

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -4,7 +4,7 @@ use crate::system_properties::SystemProperties;
 use crate::configuration::SessionConfig;
 
 
-fn get_inlet_bernoulli_point() -> BernoulliPoint{
+pub(crate)fn get_inlet_bernoulli_point() -> BernoulliPoint{
 
     let the_file = "../input/inlet.json";
     let inlet_stream_line_points: BernoulliPoint = serde_json::from_str(the_file)
@@ -13,7 +13,7 @@ fn get_inlet_bernoulli_point() -> BernoulliPoint{
     return inlet_stream_line_points
 }
 
-fn get_out_bernoulli_point() -> BernoulliPoint{
+pub(crate)fn get_outlet_bernoulli_point() -> BernoulliPoint{
 
     let the_file = "../input/outlet.json";
     let outlet_stream_line_points: BernoulliPoint = serde_json::from_str(the_file)
@@ -22,7 +22,7 @@ fn get_out_bernoulli_point() -> BernoulliPoint{
     return outlet_stream_line_points
 }
 
-fn get_system_properties() -> SystemProperties {
+pub(crate)fn get_system_properties() -> SystemProperties {
     let the_file = "../input/system_definition.json";
     let stream_line_points: SystemProperties = serde_json::from_str(the_file)
         .expect("JSON was not well-formatted");
@@ -30,7 +30,7 @@ fn get_system_properties() -> SystemProperties {
     return stream_line_points
 }
 
-fn get_session_configuration() -> SessionConfig {
+pub(crate) fn get_session_configuration() -> SessionConfig {
     let the_file = "../config/configuration.json";
     let config: SessionConfig = serde_json::from_str(the_file)
         .expect("JSON was not well-formatted");
