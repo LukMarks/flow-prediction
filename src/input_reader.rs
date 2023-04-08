80b9c6663d6b272342300f026ae1748284238b7d
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-08 07:16:05 -0300

    Fixed dependencies injections

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -1,9 +1,11 @@
 use serde::Deserialize;
+use crate::bernoulli_point::BernoulliPoint;
+use crate::system_properties::SystemProperties;
 
-fn get_bernoulli_point() -> BernoulliPoints{
+fn get_bernoulli_point() -> BernoulliPoint{
 
     let the_file = "../input/stream_line_property.json";
-    let stream_line_points: BernoulliPoints = serde_json::from_str(the_file)
+    let stream_line_points: BernoulliPoint = serde_json::from_str(the_file)
         .expect("JSON was not well-formatted");
 
     return stream_line_points
@@ -11,8 +13,8 @@ fn get_bernoulli_point() -> BernoulliPoints{
 
 fn get_system_properties() -> SystemProperties {
     let the_file = "../input/system_definition.json";
-    let stream_line_points: BernoulliPoints = serde_json::from_str(the_file)
+    let stream_line_points: SystemProperties = serde_json::from_str(the_file)
         .expect("JSON was not well-formatted");
 
-    return SystemProperties
+    return stream_line_points
 }
