5426abfb630a2f7714d08798768892970892554c
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-10 10:55:54 -0300

    Added streamline paths properties

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,4 +2,7 @@
 pub(crate) struct SessionConfig{
     save_result_csv: bool,
     show_plot: bool,
+    pub(crate) inlet_file_path: String,
+    pub(crate) outlet_file_path: String,
+    pub(crate) streamline_file_path: String
 }
