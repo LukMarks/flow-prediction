5ce2d48e96cbcc24d93363a5688ec1884127610c
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-10 17:42:27 -0300

    Convert fields to public

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Default, serde::Deserialize)]
 pub(crate) struct SessionConfig{
-    save_result_csv: bool,
-    show_plot: bool,
+    pub(crate) save_result_csv: bool,
+    pub(crate) show_plot: bool,
     pub(crate) inlet_file_path: String,
     pub(crate) outlet_file_path: String,
     pub(crate) streamline_file_path: String
