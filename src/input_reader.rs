d679418b8e3199fb7b3c50030ac26178277a5d66
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-10 11:34:27 -0300

    Pass bernoullipoint pash dynamiclly

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -2,37 +2,43 @@ use serde::Deserialize;
 use crate::bernoulli_point::BernoulliPoint;
 use crate::system_properties::SystemProperties;
 use crate::configuration::SessionConfig;
+use std::env;
+use std::fs;
 
+pub(crate)fn get_inlet_bernoulli_point(inlet_file_path: &str) -> BernoulliPoint{
 
-pub(crate)fn get_inlet_bernoulli_point() -> BernoulliPoint{
-
-    let the_file = "../input/inlet.json";
-    let inlet_stream_line_points: BernoulliPoint = serde_json::from_str(the_file)
+    let inlet_stream_line_points: BernoulliPoint = serde_json::from_str(inlet_file_path)
         .expect("JSON was not well-formatted");
 
     return inlet_stream_line_points
 }
 
-pub(crate)fn get_outlet_bernoulli_point() -> BernoulliPoint{
+pub(crate)fn get_outlet_bernoulli_point(outlet_file_path: &str) -> BernoulliPoint{
 
-    let the_file = "../input/outlet.json";
-    let outlet_stream_line_points: BernoulliPoint = serde_json::from_str(the_file)
+    let outlet_stream_line_points: BernoulliPoint = serde_json::from_str(outlet_file_path)
         .expect("JSON was not well-formatted");
 
     return outlet_stream_line_points
 }
 
-pub(crate)fn get_system_properties() -> SystemProperties {
-    let the_file = "../input/system_definition.json";
-    let stream_line_points: SystemProperties = serde_json::from_str(the_file)
+pub(crate)fn get_system_properties(streamline_file_path: &str) -> SystemProperties {
+
+
+    let system_properties_file_content =  fs::read_to_string(streamline_file_path)
+        .expect("Should have been able to read the file");
+
+    let streamline_properties: SystemProperties = serde_json::from_str(&*system_properties_file_content)
         .expect("JSON was not well-formatted");
 
-    return stream_line_points
+    return streamline_properties
 }
 
-pub(crate) fn get_session_configuration() -> SessionConfig {
-    let the_file = "../config/configuration.json";
-    let config: SessionConfig = serde_json::from_str(the_file)
+pub(crate) fn get_session_configuration(configuration_file_path: &str) -> SessionConfig {
+
+    let config_file_content =  fs::read_to_string(configuration_file_path)
+        .expect("Should have been able to read the file");
+
+    let config: SessionConfig = serde_json::from_str(&*config_file_content)
         .expect("JSON was not well-formatted");
 
     return config
