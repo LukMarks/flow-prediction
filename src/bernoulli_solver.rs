b1d2aded73f24f1073b5321c83e81ddc8f4d48fb
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-11 19:01:00 -0300

    Added pressure solver

--- src/bernoulli_solver.rs
+++ src/bernoulli_solver.rs
@@ -3,3 +3,14 @@ use crate::system_properties::SystemProperties;
 
 #[derive(Debug, serde::Deserialize)]
 pub(crate) struct BernoulliSolver {}
+
+impl BernoulliSolver {
+    fn get_exit_pressure(entry_point: BernoulliPoint, exit_point: BernoulliPoint,
+                         properties: SystemProperties) -> f32{
+        let exit_pressure: f32 = entry_point.pressure +
+            properties.density*(0.5*(entry_point.velocity.powi(2) - exit_point.velocity.powi(2)) +
+                properties.gravity_acceleration*(entry_point.height-exit_point.height));
+
+        return exit_pressure
+    }
+}
