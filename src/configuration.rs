0101fe2c7285937b6d1241450124af4a2e7f76cb
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-11 20:21:53 -0300

    Added solver field

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,8 @@
-#[derive(Debug, Default, serde::Deserialize)]
+#[derive(Debug, serde::Deserialize)]
 pub(crate) struct SessionConfig{
     pub(crate) save_result_csv: bool,
     pub(crate) show_plot: bool,
+    pub(crate) solver_mode: String,
     pub(crate) inlet_file_path: String,
     pub(crate) outlet_file_path: String,
     pub(crate) streamline_file_path: String
