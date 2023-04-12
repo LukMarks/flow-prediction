550d87357797541e54d5253f21778148972f8ce5
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-12 19:14:38 -0300

    Update mod reference

--- src/bernoulli/mod.rs
+++ src/bernoulli/mod.rs
@@ -2,10 +2,9 @@ mod bernoulli_solver;
 mod bernoulli_point;
 mod flow_properties;
 
-pub mod bernoulli_system{
-    use bernoulli_point::BernoulliPoint;
-    use bernoulli_solver::BernoulliSolver;
-    use flow_properties::SystemProperties;
-    use crate::bernoulli::flow_properties;
+pub(crate) mod bernoulli_system{
+    use super::bernoulli_point::BernoulliPoint;
+    use super::bernoulli_solver::BernoulliSolver;
+    use super::flow_properties::SystemProperties;
 }
 
