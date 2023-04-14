5f6d2b0fc1e11b39d885a8ce5db8c43a9f1340ea
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-14 10:09:04 -0300

    Removed unused coe

--- src/bernoulli/mod.rs
+++ src/bernoulli/mod.rs
@@ -7,8 +7,3 @@ use crate::bernoulli::bernoulli_point::BernoulliPoint;
 use crate::bernoulli::bernoulli_solver::BernoulliSolver;
 use crate::bernoulli::flow_properties::SystemProperties;
 
-// pub mod bernoulli_system{
-//     use super::bernoulli_point::BernoulliPoint;
-//     use super::bernoulli_solver::BernoulliSolver;
-//     use super::flow_properties::SystemProperties;
-// }
