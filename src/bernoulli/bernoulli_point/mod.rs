b5a9ae5f0936f41b88ffcad00e93ff03a2428c26
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-17 17:28:09 -0300

    Implement copy and clone trait

--- src/bernoulli/bernoulli_point/mod.rs
+++ src/bernoulli/bernoulli_point/mod.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, Default, serde::Deserialize)]
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize, Copy, Clone)]
 pub struct BernoulliPoint {
     pub(crate) pressure : f32,
     pub(crate) velocity: f32,
