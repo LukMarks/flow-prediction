63473d49600ad2f68af7752868607ee5cb56f998
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-17 18:03:52 -0300

    Updated struct field names

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,10 @@
 #[derive(Debug, serde::Deserialize)]
 pub(crate) struct SessionConfig{
-    pub(crate) save_result_csv: bool,
-    pub(crate) show_plot: bool,
+    pub(crate) save_result: bool,
+    pub(crate) show_result: bool,
     pub(crate) solver_mode: String,
     pub(crate) inlet_file_path: String,
     pub(crate) outlet_file_path: String,
-    pub(crate) streamline_file_path: String
+    pub(crate) streamline_file_path: String,
+    pub(crate) result_file_path: String
 }
