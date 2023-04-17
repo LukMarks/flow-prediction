0eb114fcf9b691db7984e42c1b2656c7ecde57c3
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-17 18:04:15 -0300

    Added function to show and save results

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,19 @@
-pub(crate) fn save_result_plot(){
-    //save result as a plot
+use crate::bernoulli::bernoulli_point::BernoulliPoint;
+use serde;
+use std::fs;
+
+
+pub(crate) fn show_result(result: &BernoulliPoint){
+    let result_as_string = bernoulli_point_to_string(result);
+    println!("Exit Bernoulli Point:\n{}", result_as_string)
 }
 
-pub(crate) fn save_result_data(){
-    //save data as csv
+pub(crate) fn save_result_data(result_file_path: &str, result: &BernoulliPoint){
+    let result_as_string = bernoulli_point_to_string(result);
+    fs::write(result_file_path, result_as_string).expect("Unable to write file");
+}
+
+fn bernoulli_point_to_string(bernoulli_point: &BernoulliPoint)->String{
+    let result_as_string = serde_json::to_string(&bernoulli_point).unwrap();
+    return result_as_string;
 }
