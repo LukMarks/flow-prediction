9d272b6287639dcf1e880cbe67f89ee7693bd5d7
LukMarks <lucas.mmoreno@hotmail.com>
2023-04-25 08:08:47 -0300

    Removed redundant method

--- src/input_reader.rs
+++ src/input_reader.rs
@@ -5,26 +5,15 @@ use crate::configuration::SessionConfig;
 
 use std::fs;
 
-pub(crate) fn get_inlet_bernoulli_point(inlet_file_path: &str) -> BernoulliPoint{
+pub(crate) fn get_surface_bernoulli_point(surface_file_path: &str) -> BernoulliPoint{
 
-    let inlet_file_content =  fs::read_to_string(inlet_file_path)
+    let surface_file_content =  fs::read_to_string(surface_file_path)
         .expect("Should have been able to read the file");
 
-    let inlet_stream_line_points: BernoulliPoint = serde_json::from_str(&*inlet_file_content)
+    let surface_stream_line_points: BernoulliPoint = serde_json::from_str(&*surface_file_content)
         .expect("JSON was not well-formatted");
 
-    return inlet_stream_line_points
-}
-
-pub(crate) fn get_outlet_bernoulli_point(outlet_file_path: &str) -> BernoulliPoint{
-
-    let outlet_file_content =  fs::read_to_string(outlet_file_path)
-        .expect("Should have been able to read the file");
-
-    let outlet_stream_line_points: BernoulliPoint = serde_json::from_str(&*outlet_file_content)
-        .expect("JSON was not well-formatted");
-
-    return outlet_stream_line_points
+    return surface_stream_line_points
 }
 
 pub(crate) fn get_system_properties(streamline_file_path: &str) -> SystemProperties {
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ pub mod input_reader;
 
 use std::env;
 use crate::configuration::SessionConfig;
-use crate::input_reader::{get_session_configuration, get_inlet_bernoulli_point,
-                          get_outlet_bernoulli_point, get_system_properties};
+use crate::input_reader::{get_session_configuration, get_surface_bernoulli_point,
+                          get_system_properties};
 
 use crate::bernoulli::bernoulli_point::BernoulliPoint;
 use crate::bernoulli::bernoulli_solver::BernoulliSolver;
@@ -19,8 +19,8 @@ fn main() {
 
     let stream_properties:SystemProperties = get_system_properties(&session_config.streamline_file_path);
 
-    let streamline_exit:BernoulliPoint = get_outlet_bernoulli_point(&session_config.outlet_file_path);
-    let streamline_entry:BernoulliPoint = get_inlet_bernoulli_point(&session_config.inlet_file_path);
+    let streamline_exit:BernoulliPoint = get_surface_bernoulli_point(&session_config.outlet_file_path);
+    let streamline_entry:BernoulliPoint = get_surface_bernoulli_point(&session_config.inlet_file_path);
 
     let flow_result:BernoulliPoint = BernoulliSolver::solve(session_config.solver_mode,
     streamline_entry, streamline_exit, stream_properties);
